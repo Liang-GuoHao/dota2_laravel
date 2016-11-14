e0fd7fc682c507ef92579b9d47ad0642ca79e9b9
liangguohao <247509257@qq.com>
2016-11-14 22:48:06 +0800

    install laravel

---
+++ app/Http/Controllers/DeploymentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DeploymentController extends Controller
+{
+	public function deploy(Request $request)
+	{
+		$signature = $request->header('X-Hub-Signature');
+		$payload = file_get_contents('php://input');
+		if ($this->isFromGithub($payload, $signature)) {
+			shell_exec('cd /data/www/dota2_laravel && git pull origin master');
+			http_response_code(200);
+		} else {
+			abort(403);
+		}
+	}
+
+	private function isFromGithub($payload, $signature)
+	{
+		return 'sha1=' . hash_hmac('sha1', $payload, env('GITHUB_DEPLOY_TOKEN'), false) === $signature;
+	}
+}
+
