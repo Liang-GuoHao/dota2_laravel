6aa9acaacc9543a69a8745dd283f1f2a31f360ee
liangguohao <1803559929@qq.com>
2016-11-14 23:13:46 +0800

    test git auto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel for Dota2
                 </div>
 
                 <div class="links">
