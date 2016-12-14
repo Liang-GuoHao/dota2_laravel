5f223a07b9e6ea950a4646deca87900bd0d17a9a
liangguohao <1803559929@qq.com>
2016-12-14 11:11:15 +0800

    test webhook

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel for Dota2
+                    Dota2
                 </div>
 
                 <div class="links">
